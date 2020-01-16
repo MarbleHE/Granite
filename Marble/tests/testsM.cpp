4e1454b046641af6dde123357e2ec565f2ab01eb
Mario Stöckli <stmario@ethz.ch>
2020-01-16 16:56:50 +0100

    Changed from copy paste AST to proper submodule AST. Started implementation of M.

--- Marble/tests/testsM.cpp
+++ Marble/tests/testsM.cpp
@@ -1,4 +1,13 @@
 //
 // Created by mario on 08.01.20.
 //
+#include <gtest/gtest.h>
+#include "Ast.h"
 
+TEST(BasicTest, Eq){
+    ASSERT_EQ(1,1);
+}
+
+TEST(BasicAst, Eq){
+    ASSERT_NO_THROW(Ast());
+}
