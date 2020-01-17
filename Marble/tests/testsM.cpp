14e55a3c088f121d444b2fb2223e80fcf4b269f1
Mario Stöckli <stmario@ethz.ch>
2020-01-17 17:44:42 +0100

    Adding basic test for make_AST.

--- Marble/tests/testsM.cpp
+++ Marble/tests/testsM.cpp
@@ -1,9 +1,15 @@
 //
 // Created by mario on 08.01.20.
 //
-#include <gtest/gtest.h>
+#include <fstream>
+#include "gtest/gtest.h"
 #include "Ast.h"
+#include "AbstractExpr.h"
+#include "AbstractStatement.h"
 #include "M.hpp"
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
 
 TEST(BasicTest, GoogleTestImport){
     ASSERT_EQ(1,1);
@@ -15,4 +21,22 @@ TEST(BasicAst, AstImport){
 
 TEST(BasicM, MImport){
     ASSERT_NO_THROW(Marble::M());
+}
+
+void empty_func(){
+}
+
+class MTest : public ::testing::Test {
+    protected:
+        std::function <void()> f;
+};
+
+TEST_F(MTest, EmptyBodyAST){
+    f = empty_func;
+    Ast ast = Marble::M::make_AST(f);
+    // TODO: change to relative path
+    std::ifstream file("/home/mario/Desktop/Granite/Marble/tests/expected_output/empty_func.json");
+    json expected_j = json::parse(file);
+    auto rn = ast.getRootNode();
+    EXPECT_EQ(expected_j.dump(), rn->toString());
 }
